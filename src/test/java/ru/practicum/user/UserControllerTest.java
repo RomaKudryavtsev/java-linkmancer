454c324937d3aa3aa26b5d582721b11058b3c583
Roman Kudryavtsev <kudryavtsevroman@mail.ru>
2023-04-08 01:16:12 +0300

    add UserControllerTest

---
+++ src/test/java/ru/practicum/user/UserControllerTest.java
@@ -0,0 +1,68 @@
+package ru.practicum.user;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import java.nio.charset.StandardCharsets;
+
+import static org.hamcrest.Matchers.is;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@ExtendWith(MockitoExtension.class)
+class UserControllerTest {
+    @Mock
+    private UserService userService;
+
+    @InjectMocks
+    private UserController controller;
+
+    private final ObjectMapper mapper = new ObjectMapper();
+
+    private MockMvc mvc;
+
+    private UserDto userDto;
+
+    @BeforeEach
+    void setUp() {
+        mvc = MockMvcBuilders
+                .standaloneSetup(controller)
+                .build();
+
+        userDto = new UserDto(
+                1L,
+                "john.doe@mail.com",
+                "John",
+                "Doe",
+                "2022.07.03 19:55:00",
+                UserState.ACTIVE);
+    }
+
+    @Test
+    void saveNewUser() throws Exception {
+        when(userService.saveUser(any()))
+                .thenReturn(userDto);
+
+        mvc.perform(post("/users")
+                        .content(mapper.writeValueAsString(userDto))
+                        .characterEncoding(StandardCharsets.UTF_8)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id", is(userDto.getId()), Long.class))
+                .andExpect(jsonPath("$.firstName", is(userDto.getFirstName())))
+                .andExpect(jsonPath("$.lastName", is(userDto.getLastName())))
+                .andExpect(jsonPath("$.email", is(userDto.getEmail())));
+    }
+}
