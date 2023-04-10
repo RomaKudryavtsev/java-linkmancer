49ba49dc69021588c3123dc6755df85a3bdebd98
Roman Kudryavtsev <kudryavtsevroman@mail.ru>
2023-04-10 15:46:51 +0300

    add search item test

--- src/test/java/ru/practicum/item/ItemServiceImplTest.java
+++ src/test/java/ru/practicum/item/ItemServiceImplTest.java
@@ -1,5 +1,6 @@
 package ru.practicum.item;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,6 +14,7 @@ import org.springframework.test.context.web.WebAppConfiguration;
 import ru.practicum.config.AppConfig;
 import ru.practicum.config.PersistenceConfig;
 import ru.practicum.config.WebConfig;
+import ru.practicum.item.request_search.SearchRequest;
 import ru.practicum.user.UserDto;
 import ru.practicum.user.UserService;
 import ru.practicum.user.UserState;
@@ -21,6 +23,8 @@ import javax.persistence.EntityManager;
 import javax.persistence.TypedQuery;
 import javax.transaction.Transactional;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Set;
 
 import static org.hamcrest.CoreMatchers.notNullValue;
@@ -39,6 +43,9 @@ public class ItemServiceImplTest {
     private final ItemService itemService;
     private final UserService userService;
 
+    private UserDto savedUser;
+    private final List<ItemDto> itemDtoList = new ArrayList<>();
+
     @Autowired
     public ItemServiceImplTest(EntityManager em, ItemService itemService, @Qualifier("userServiceImpl") UserService userService) {
         this.em = em;
@@ -46,25 +53,29 @@ public class ItemServiceImplTest {
         this.userService = userService;
     }
 
-    @Test
-    void saveItem() {
+    @BeforeEach
+    void beforeEach() {
         // given
         UserDto userDto = makeUserDto("some@email.com", "Peter", "Pan");
-        ItemDto itemDto = new ItemDto();
-        itemDto.setTags(Set.of("Trading", "IT"));
-        itemDto.setUrl("https://algotrading101.com/learn/deribit-api-guide/");
-
+        ItemDto itemDto1 = makeItemDto("https://algotrading101.com/learn/deribit-api-guide/",
+                Set.of("Trading", "IT"));
+        ItemDto itemDto2 = makeItemDto("https://practicum.yandex.ru/java-developer/",
+                Set.of("Education", "IT", "Java"));
+        itemDtoList.addAll(List.of(itemDto1, itemDto2));
         // when
-        UserDto savedUserDto = userService.saveUser(userDto);
-        itemService.addNewItem(savedUserDto.getId(), itemDto);
+        savedUser = userService.saveUser(userDto);
+        itemDtoList.forEach(itemDto -> itemService.addNewItem(savedUser.getId(), itemDto));
+    }
 
+    @Test
+    void saveItem() {
         // then
         TypedQuery<Item> queryItem = em.createQuery("Select i from Item i where i.url = :url", Item.class);
-        Item item = queryItem.setParameter("url", itemDto.getUrl())
+        Item item = queryItem.setParameter("url", itemDtoList.get(0).getUrl())
                 .getSingleResult();
 
         assertThat(item.getId(), notNullValue());
-        assertThat(item.getUrl(), equalTo(itemDto.getUrl()));
+        assertThat(item.getUrl(), equalTo(itemDtoList.get(0).getUrl()));
         assertThat(item.getMimeType(), equalTo("text"));
         assertThat(item.getHasImage(), equalTo(true));
         assertThat(item.getHasVideo(), equalTo(false));
@@ -73,13 +84,36 @@ public class ItemServiceImplTest {
         assertThat(item.getTags(), notNullValue());
     }
 
+    @Test
+    void searchItem() {
+        SearchRequest request = makeSearchRequest(savedUser.getId(), "UNREAD", "ARTICLE", "NEWEST",
+                5, List.of("Trading"));
+        List<ItemDto> foundItems = itemService.searchWithFilters(request);
+        assertThat(foundItems.size(), equalTo(1));
+        assertThat(foundItems.get(0).getId(), notNullValue());
+        assertThat(foundItems.get(0).getUrl(), equalTo(itemDtoList.get(0).getUrl()));
+        assertThat(foundItems.get(0).getUserId(), equalTo(savedUser.getId()));
+        assertThat(foundItems.get(0).getTags(), notNullValue());
+    }
+
     private UserDto makeUserDto(String email, String firstName, String lastName) {
         UserDto dto = new UserDto();
         dto.setEmail(email);
         dto.setFirstName(firstName);
         dto.setLastName(lastName);
         dto.setState(UserState.ACTIVE);
-
         return dto;
+    }
+
+    private ItemDto makeItemDto(String url, Set<String> tags) {
+        ItemDto itemDto = new ItemDto();
+        itemDto.setUrl(url);
+        itemDto.setTags(tags);
+        return itemDto;
+    }
+
+    private SearchRequest makeSearchRequest(Long userId, String state, String contentType, String sort,
+                                            Integer limit, List<String> tags) {
+        return new SearchRequest(userId, state, contentType, sort, limit, tags);
     }
 }
